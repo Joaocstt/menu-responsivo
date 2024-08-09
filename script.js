40b51535d51e9bf31611becb605a748903ab8057
unknown <jnc201820@gmail.com>
2024-08-08 23:45:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector(".menu-celular");
+
+menu.addEventListener("click", () => {
+    let menu_desktop = document.querySelector(".menu");
+
+    menu_desktop.classList.toggle("active")
+
+})
